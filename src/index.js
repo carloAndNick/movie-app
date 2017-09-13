8993c4b6b7584511a2e824bcf8f50d1070593f3e
Uglygrayduck <Nicholasgswan@gmail.com>
2017-09-13 17:06:49 -0500

    made functions for displaying movies in html, made button to add movies

--- src/index.js
+++ src/index.js
@@ -12,15 +12,36 @@ const getMovies = require('./getMovies.js');
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   document.getElementById("loading").innerHTML = 'Here are all the movies:'
-  movies.forEach(({title, rating, id}) => {
-      document.getElementById("movie-list").innerHTML +=`<li>id#${id} - ${title} - rating: ${rating}</li>`
+  movies.forEach(({title, rating}) => {
+      addMovieToHtml(title, rating)
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
 
-function movieList() {
-    document.getElementById("movie-list").innerHtml =`id#${id} - ${title} - rating: ${rating}`
-};
+//this is the event function for clicking the add movie button
+function addAMovie(){
+    let movieTitle= document.getElementById("title").value;
+    let movieRating= document.getElementById("rating").value;
+    console.log(movieTitle);
+    console.log(movieRating);
+    addMovieToHtml(movieTitle, movieRating)
+    // let movieObj = "{'title':'movieTitle','rating': 'movieRating'}";
+    // // let parsedMovieObj =JSON.parse(movieObj)
+    // let fetchOptions = {
+    //     method: "POST",
+    //     body: movieObj,
+    // };
+    // fetch("/api/movies", fetchOptions)
+    //     .then((response)=>console.log(response.json))
+}
+//event listener for button
+document.getElementById("button").addEventListener("click", addAMovie);
+
+//function to add movies to the html
+function addMovieToHtml( title, rating){
+    document.getElementById("movie-list").innerHTML +=`<li>${title} - rating: ${rating}</li>`
+}
+
 
