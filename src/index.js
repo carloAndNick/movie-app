e102b7a37a5148f0bebbf4607e1e0c15119628da
Bolimbo <carlo.cappellini@yahoo.com>
2017-09-13 15:37:16 -0500

    displayed all movies using innerHTML

--- src/index.js
+++ src/index.js
@@ -12,9 +12,13 @@ const getMovies = require('./getMovies.js');
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+      document.getElementById("movie-list").innerHTML +=`<li>id#${id} - ${title} - rating: ${rating}</li>`
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+function movieList() {
+    document.getElementById("movie-list").innerHtml =`id#${id} - ${title} - rating: ${rating}`
+};
