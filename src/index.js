710216bad1ad3087ec98f45454753a1ac0e21996
Uglygrayduck <Nicholasgswan@gmail.com>
2017-09-14 15:16:03 -0500

    made an edit function

--- src/index.js
+++ src/index.js
@@ -9,16 +9,22 @@ sayHello('World');
  */
 const getMovies = require('./getMovies.js');
 // document.getElementById("edit-form").setAttribute("hidden", "true");
-
-getMovies().then((movies) => {
-    console.log('Here are all the movies:');
-    document.getElementById("loading").innerHTML = 'Here are all the movies:'
-    movies.forEach(({title, rating}) => {
-        addMovieToHtml(title, rating)
+function displayAllMovies() {
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+        document.getElementById("loading").innerHTML = 'Here are all the movies:'
+        movies.forEach(({title, rating}) => {
+            addMovieToHtml(title, rating)
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
     });
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
-    console.log(error);
+}
+displayAllMovies();
+const header= new Headers({
+    'Accept':'application/json',
+    'Content-Type': 'application/json'
 });
 
 //this is the event function for clicking the add movie button
@@ -28,9 +34,7 @@ getMovies().then((movies) => {
 function addAMovie(){
     let movieTitle= document.getElementById("title").value;
     let movieRating= document.getElementById("rating").value;
-    let header= new Headers({
-        'Content-Type': 'application/json'
-    });
+
     let movieObj = {title:movieTitle,rating:movieRating};
     console.log(movieObj);
     let fetchOptions = {
@@ -60,6 +64,9 @@ console.log(document.getElementById("edit-movie").innerHTML);
 
 //event handler for edit button
 function editButton() {
+    reHide("movie-list-container");
+    reHide("new-movie-form");
+    reHide("edit-button");
     unHide("edit-form");
     unHide("edit-this-movie");
     getMovies().then((movies) => {
@@ -78,8 +85,11 @@ document.getElementById("edit-button").addEventListener("click", editButton);
 
 //event handler for edit this movie button
 function editThisMovie(){
+    reHide("edit-form");
+    reHide("edit-this-movie");
     unHide("edit-submit-form");
-    let movieToEditIndex= (document.getElementById("edit-movie").value)-1;
+    let movieToEditId= (document.getElementById("edit-movie").value);
+    let movieToEditIndex= movieToEditId-1;
     console.log(movieToEditIndex);
     getMovies().then((movies)=>{
         console.log(movies[movieToEditIndex]);
@@ -88,20 +98,45 @@ function editThisMovie(){
         document.getElementById("edit-title").value = movieToEditTitle;
         document.getElementById("edit-rating").value = movieToEditRating
 
-    })
-
+    });
 
 }
 //event listener for submit form
 document.getElementById("edit-this-movie").addEventListener("click", editThisMovie);
 
+//button to submit edits
+function submitEdit(){
+    let editedMovieTitle = document.getElementById("edit-title").value;
+    let editedMovieRating = document.getElementById("edit-rating").value;
+    let movieToEditId = document.getElementById("edit-movie").value;
+    let editedMovieObj = {title:editedMovieTitle,rating:editedMovieRating,id:movieToEditId};
+    console.log(editedMovieObj);
+    let fetchOptions = {
+        method: "PUT",
+        body: JSON.stringify(editedMovieObj),
+        headers: header
+    };
+    fetch(`/api/movies/${movieToEditId}`,fetchOptions).then(()=>{
+        document.getElementById("movie-list").innerHTML = "";
+       unHide("movie-list-container");
+        reHide("edit-submit-form");
+        unHide("new-movie-form");
+        unHide("edit-button")
+    }).then(()=>{displayAllMovies()});
+
+}
+
+//event listener for submitting edits
+document.getElementById("confirm-edit-button").addEventListener("click", submitEdit);
+
 
 //function to unhide things. the element id should be in the form of a string
 function unHide(elementId) {
     document.getElementById(elementId).setAttribute("hidden", "false");
-    document.getElementById(elementId).style.display = "inline";
+    document.getElementById(elementId).style.display = "initial";
 }
 function reHide(elementId){
     document.getElementById(elementId).setAttribute("hidden", "true");
     document.getElementById(elementId).style.display = "none";
 }
+
